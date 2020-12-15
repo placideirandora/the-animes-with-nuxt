6f464d59fd3ce8fb027cd6fcfce83ef673dd11fe
Placide IRANDORA <placideirandora@gmail.com>
2020-12-15 16:50:53 +0200

    Implement the General Layout of the App

---
+++ layouts/default.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Header from "./partials/Header";
+import Footer from "./partials/Footer";
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start();
+
+      setTimeout(() => this.$nuxt.$loading.finish(), 500);
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+html {
+  font-family: "Roboto";
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  color: #0d0221;
+
+  &,
+  body {
+    width: 100%;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+  }
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+h3 {
+  margin-bottom: 10px;
+}
+
+main {
+  padding: 0 20em;
+}
+</style>
---
+++ layouts/partials/Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <header>
+    <h3 class="logo"><nuxt-link to="/">The Animes</nuxt-link></h3>
+    <ul>
+      <li><nuxt-link to="/action">Action</nuxt-link></li>
+      <li><nuxt-link to="/love">Love</nuxt-link></li>
+      <li><nuxt-link to="/comedy">Comedy</nuxt-link></li>
+      <li><nuxt-link to="/horror">Horror</nuxt-link></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+header {
+  background: #386fa4;
+  color: #fff;
+  width: 100%;
+  height: 5em;
+  padding: 0 20em;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 2em;
+
+  h3.logo {
+    font-size: 1.8em;
+    font-weight: 300;
+    text-transform: uppercase;
+    padding-top: 15px;
+
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+
+    li {
+      &,
+      a {
+        padding: 1em;
+      }
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+
+      &:hover {
+        border-bottom: solid 1px #fff;
+        transition: all 0.2s ease-in-out;
+      }
+    }
+  }
+}
+</style>
