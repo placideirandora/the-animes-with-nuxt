4a1258380f2eceebae4a22c9f9d2ccbf16b40ec3
Placide IRANDORA <placideirandora@gmail.com>
2020-12-16 11:13:10 +0200

    Implement a simple Dummy Authentication on Pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,12 +7,10 @@
 
 <script>
 import Header from "./partials/Header";
-import Footer from "./partials/Footer";
 
 export default {
   components: {
     Header,
-    Footer,
   },
   mounted() {
     this.$nextTick(() => {
--- layouts/partials/Header.vue
+++ layouts/partials/Header.vue
@@ -1,17 +1,37 @@
 <template>
   <header>
-    <h3 class="logo"><nuxt-link to="/">The Animes</nuxt-link></h3>
-    <ul>
+    <p class="logo"><nuxt-link to="/">The Animes</nuxt-link></p>
+    <ul v-if="authenticated">
       <li><nuxt-link to="/action">Action</nuxt-link></li>
       <li><nuxt-link to="/love">Love</nuxt-link></li>
       <li><nuxt-link to="/comedy">Comedy</nuxt-link></li>
       <li><nuxt-link to="/horror">Horror</nuxt-link></li>
+      <li><a href @click="onLogout">Logout</a></li>
+    </ul>
+    <ul v-if="!authenticated">
+      <li><nuxt-link to="/login">Login</nuxt-link></li>
+      <li class="register-btn">
+        <nuxt-link to="/register">Register</nuxt-link>
+      </li>
     </ul>
   </header>
 </template>
 
 <script>
-export default {};
+import { mapGetters, mapMutations } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["authenticated"]),
+  },
+  methods: {
+    ...mapMutations(["setAuthenticated"]),
+    onLogout() {
+      this.setAuthenticated(false);
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -27,15 +47,16 @@ header {
   align-items: center;
   margin-bottom: 2em;
 
-  h3.logo {
-    font-size: 1.8em;
+  .logo {
+    font-size: x-large;
     font-weight: 300;
     text-transform: uppercase;
-    padding-top: 15px;
+    padding-top: 8px;
 
     a {
       text-decoration: none;
       color: inherit;
+      cursor: pointer;
     }
   }
 
@@ -47,20 +68,22 @@ header {
     flex-direction: row;
 
     li {
+      margin: 5px;
+
       &,
       a {
-        padding: 1em;
+        padding: 0.8rem;
       }
 
       a {
         text-decoration: none;
         color: inherit;
       }
+    }
 
-      &:hover {
-        border-bottom: solid 1px #fff;
-        transition: all 0.2s ease-in-out;
-      }
+    .register-btn {
+      border: 1px solid #fff;
+      border-radius: 5px;
     }
   }
 }
