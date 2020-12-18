1660f5d1e7f81d547633c9b4c38490883ff75f7c
Placide IRANDORA <placideirandora@gmail.com>
2020-12-18 17:16:06 +0200

    Change the Header background color

--- layouts/partials/Header.vue
+++ layouts/partials/Header.vue
@@ -37,7 +37,18 @@ export default {
 
 <style lang="scss">
 header {
-  background: #386fa4;
+  background: #02aab0; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to top,
+    #00cdac,
+    #02aab0
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to top,
+    #00cdac,
+    #02aab0
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
   color: #fff;
   width: 100%;
   height: 5em;
